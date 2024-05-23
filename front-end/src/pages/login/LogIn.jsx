d18a5d80976f57006b8c838749886317a121cf62
sacha11 <s.j.g_97@hotmail.com>
2024-05-23 20:24:58 -0300

    login page updated

--- front-end/src/pages/login/LogIn.jsx
+++ front-end/src/pages/login/LogIn.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const LogIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login({ username, password })
+  }
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -9,25 +17,31 @@ const LogIn = () => {
           Log In  
           <span className='text-blue-600 pl-3'>iMessenger</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text text-white'>Username</span>
             </label>
-            <input 
+            <input
+              value={username} 
               type="text" 
               placeholder='Enter Username' 
-              className='w-full input input-bordered h-10 text-black' />
+              className='w-full input input-bordered h-10 text-black' 
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
 
           <div>
             <label className='label'>
               <span className='text-base label-text text-white'>Password</span>
             </label>
-            <input 
+            <input
+              value={password}
               type="password" 
               placeholder='Enter Password' 
-              className='w-full input input-bordered h-10 text-black' />
+              className='w-full input input-bordered h-10 text-black'
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <Link to={'/signup'} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
             Don't have an account?
