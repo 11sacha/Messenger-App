bb408aeb960110449196436e1cf703741de8277b
sacha11 <s.j.g_97@hotmail.com>
2024-05-25 13:57:49 -0300

    updated Conversations component and added emojis file

--- front-end/src/components/Sidebar/Conversations.jsx
+++ front-end/src/components/Sidebar/Conversations.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations'
+import getRandomEmoji from '../../utils/emojis';
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
   return (
     <div className='py-1 flex flex-col flex-1 overflow-auto'>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1} 
+        />
+      ))}
+
+
+        {loading ? <span className='loading loading-spinner'></span> : null}
     </div>
   )
 }
