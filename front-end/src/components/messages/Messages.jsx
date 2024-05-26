cc8afb341fe489c094dce73ec48cc4ea3071e0d5
sacha11 <s.j.g_97@hotmail.com>
2024-05-26 13:42:09 -0300

    updated messages components

--- front-end/src/components/messages/Messages.jsx
+++ front-end/src/components/messages/Messages.jsx
@@ -7,6 +7,10 @@ const Messages = () => {
   const {messages, loading} =  useGetMessages();
   return (
     <div className='px-4 flex-1 overflow-auto'>
+      {!loading && 
+        messages.length > 0 && 
+        messages.map((message) => <Message key={message._id} message={message} />)}
+
         {loading && [...Array(3)].map((_, idx) => <ChatSkeleton key={idx} />)}
 
         {!loading && messages.length === 0 && (
