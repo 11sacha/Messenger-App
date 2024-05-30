0cda7200063e08d31eac85055263b6e5a84b7af5
sacha11 <s.j.g_97@hotmail.com>
2024-05-30 19:43:15 -0300

    message.controller updated

--- back-end/socket/socket.js
+++ back-end/socket/socket.js
@@ -12,6 +12,10 @@ const io = new Server(server, {
     },
 });
 
+export const getReceiverId = (receiverId) => {
+    return userSocketMap[receiverId]
+};
+
 const userSocketMap = {};
 
 io.on("connection", (socket) => {
