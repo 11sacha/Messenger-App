98590ef76be95c2adfff7f24ca4e56648c409e2c
sacha11 <s.j.g_97@hotmail.com>
2024-05-30 20:47:36 -0300

    implemented useListenMessages hook and updated Messages component

--- front-end/src/components/messages/Messages.jsx
+++ front-end/src/components/messages/Messages.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
 import ChatSkeleton from '../skeleton/ChatSkeleton'
+import useListenMessages from '../../hooks/useListenMessages'
 
 const Messages = () => {
   const {messages, loading} =  useGetMessages();
+  useListenMessages();
   const lastMessageRef = useRef();
 
   useEffect(() => {
