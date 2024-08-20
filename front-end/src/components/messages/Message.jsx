f59b659df770c71d14fb220aede2384c0a63dfaa
sacha11 <s.j.g_97@hotmail.com>
2024-08-20 10:37:38 -0300

    modified the icon on tab and extractTime feature

--- front-end/src/components/messages/Message.jsx
+++ front-end/src/components/messages/Message.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useAuthContext } from '../../context/AuthContext'
 import useConversation from '../../zustand/useConversation';
-import { extracTime } from '../../utils/extractTime'
+import { extractTime } from '../../utils/extractTime'
 
 const Message = ({message}) => {
   const { authUser } = useAuthContext();
